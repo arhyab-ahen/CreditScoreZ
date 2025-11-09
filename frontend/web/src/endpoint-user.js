f6975bfc5b6edade458fdc6bcec55d61b55bb1dd
arhyab-ahen <arhyab-ahen@users.noreply.github.com>
2025-11-09 17:34:29 +0000

    Add endpoint-user.js, mutations.ts

---
+++ frontend/web/src/endpoint-user.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+interface decoratorProps { data: string }
+
+
---
+++ frontend/web/src/layouts-fixture/mutations.ts
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+
+interface ape_exportProps { data: string }
+
+
